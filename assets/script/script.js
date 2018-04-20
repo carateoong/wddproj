78fd77ee34fcdf0c17994b6b17bb942e15f85792
carateoong <35482144+carateoong@users.noreply.github.com>
2018-04-19 17:42:18 -0700

    added other files

---
+++ assets/script/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+let pics = {
+	0 : '1',
+	1 : '2',
+	2 : '3',
+	3 : '4',
+	4 : '5',
+	5 : '6',
+	6 : '7',
+	7 : '8',
+	8 : '9',
+
+}
+
+	for (i = 0; i < 3; i++) {
+		//jQuery Function Number 1
+  		$("#pic" + pics[i]).append("<img id=\"pic" + pics[i] + "\" class=\"picture\" src=\"assets/img/" + pics[i] + ".jpg\">");
+	}
+
+	for (i = 3; i < 6; i++) {
+  		$("#pic" + pics[i]).append("<img id=\"pic" + pics[i] + "\" class=\"picture\" src=\"assets/img/" + pics[i] + ".jpg\">");
+  		if (i == 5) {
+  			//jQuery Function Number 2
+  			$("#pic6 img").addClass("explore");
+  		}
+	}
+
+	for (i = 6; i < 9; i++) {
+  		$("#pic" + pics[i]).append("<img id=\"pic" + pics[i] + "\" class=\"picture\" src=\"assets/img/" + pics[i] + ".jpg\">");
+	}
+
+	//jQuery Function Number 3
+    $(".fancy-box").css("width", "22.5vw");
+    $(".fancy-box").css("height", "3vw");
+
+
+	$(".picture").click(function() {
+	console.log("ASfdfdas");
+		//jQuery Function Number 3
+		$("#overlay").show();
+		$(this).addClass("pic-click");
+	})
+
+	$("#overlay").click(function() {
+		//jQuery Function Number 4
+		$(".picture").removeClass("pic-click");
+		$(this).hide();
+	})
+
+
+});
+
+
